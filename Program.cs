f69db6dab98022c9a538f9d7aee0e5b6e9a67155
wuyeelok <wuyeelok@gmail.com>
2025-02-19 15:52:18 +0800

    1st sort

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var p = 14;
+Console.WriteLine($"Hello, World! {14}!");
+
+var names = new List<string> { "Zoo", "Peter", "Ana", "Felipe" };
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToLower()}!");
+}
+
+names.Sort(); // sort the names list and will update the names list
+foreach (var name in names)
+{
+    Console.WriteLine($"Hello {name.ToLower()}!");
+}
