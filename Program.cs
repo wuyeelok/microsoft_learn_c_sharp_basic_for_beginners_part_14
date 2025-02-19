ad35bd9a364c52bfb0a1ff8a0eaa824df0439970
wuyeelok <wuyeelok@gmail.com>
2025-02-19 16:00:51 +0800

    Sorting and finding index in list

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var p = 14;
-Console.WriteLine($"Hello, World! {14}!");
+Console.WriteLine($"Hello, World! {p}!");
 
-var names = new List<string> { "Zoo", "Peter", "Ana", "Felipe" };
+var names = new List<string> { "Zoo", "Peter", "Ana", "Felipe", "Candy" };
+names.Insert(1, "Jimmy");
 foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToLower()}!");
 }
-
-names.Sort(); // sort the names list and will update the names list
+Console.WriteLine($"Index of Ana is: {names.IndexOf("Ana")}");
+names.Sort((a, b) => b.CompareTo(a)); // sort the names list and will update the names list
+Console.WriteLine("After sort:");
 foreach (var name in names)
 {
     Console.WriteLine($"Hello {name.ToLower()}!");
 }
+Console.WriteLine($"Index of Ana is: {names.IndexOf("Ana")}");
